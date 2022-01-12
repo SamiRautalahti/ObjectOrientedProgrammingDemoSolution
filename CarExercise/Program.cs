37b8b05deab730234f2093dd8f56b877b7ad5d0a
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-12 13:19:27 +0200

    Add project files.

---
+++ CarExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
