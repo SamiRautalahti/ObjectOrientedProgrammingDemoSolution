d21c079870ef3a9ee59304f41ba786af1c8fd4ff
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-12 14:56:55 +0200

    Book exercise started.

---
+++ BookExercise/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExercise
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        // empty constructor
+        public Book()
+        {
+            this.title = string.Empty;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 0;
+        }
+
+        // constructor
+        public Book(string title)
+        {
+            this.title = title;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 0;
+        }
+
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+        }
+    }
+}
