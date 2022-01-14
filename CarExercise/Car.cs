545649bbf20753366e70e529c8cb8e1ca980ba15
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-14 13:37:18 +0200

    Car exercise updated

--- CarExercise/Car.cs
+++ CarExercise/Car.cs
@@ -9,17 +9,42 @@ namespace CarExercise
     class Car
     {
         public string color;
+        public string brand;
+        public int speed;
 
         public Car()
         {
             this.color = string.Empty;
-            //AskData();
+            this.brand = string.Empty;
+            this.speed = 0;
+            //AskData
         }
         
         public void AskData()
         {
             Console.Write("Auton väri? ");
             this.color = Console.ReadLine();
+            Console.Write("Anna auton merkki? ");
+            this.brand = Console.ReadLine();
+            Console.Write("Anna auton nopeus? ");
+            this.speed = int.Parse(Console.ReadLine());
+
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki on {this.brand}, väri on nyt {this.color} ja nopeus on {this.speed}");
+
+        }
+        public void Accelerate(int change)
+        {
+            
+            
+        }
+
+        public void Brake()
+        {
+
         }
     }
 }
--- CarExercise/Program.cs
+++ CarExercise/Program.cs
@@ -13,8 +13,14 @@ namespace CarExercise
             Console.WriteLine("Car tehtävä:");
             Car car1 = new Car();
             car1.AskData();
-            Console.WriteLine($"Auton väri on nyt {car1.color}");
+            car1.Accelerate(25);
+            Console.WriteLine($"\nNopeutta muutettu {car1.speed}");
+            car1.Brake();
+            Console.WriteLine($"\nJarrutettu, nopeus on nyt {car1.speed}");
+            car1.ShowCarInfo();
+            Console.WriteLine("\nCar tehtävä loppui!");
             Console.ReadLine();
+
         }
     }
 }
