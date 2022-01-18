4f3ce1af60acbee7ebf4d6b835b3db0de2853a54
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-18 10:47:41 +0200

    Company Exercise Updated

--- CompanyExercise/Company.cs
+++ CompanyExercise/Company.cs
@@ -8,8 +8,32 @@ namespace CompanyExercise
 {
     class Company
     {
+        public string address;
+        public int buildingYear;
+
+        // empty constructor
         public Company()
         {
+            this.address = string.Empty;
+            this.buildingYear = 0;
+        }
+
+        public Company(string address, int buildingYear)
+        {
+            this.address = address;
+            this.buildingYear = buildingYear;
+        }
+
+        // copy constructor
+        public Company(Company company)
+        {
+            this.address = company.address;
+            this.buildingYear = company.buildingYear;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Rakennuksen osoite: {this.address}, rakennusvuosi: {this.buildingYear}");
         }
     }
 }
--- CompanyExercise/Program.cs
+++ CompanyExercise/Program.cs
@@ -10,7 +10,21 @@ namespace CompanyExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Company tehtävä:");
+            Console.WriteLine("Company exercise:");
+
+            Console.WriteLine("Example: Company class");
+            Company school = new Company();
+            school.buildingYear = 1990;
+            school.address = "Skinnarilankatu 36, 53850 Lappeenranta";
+            school.PrintInfo();
+
+            Company office = new Company("Marssitie 50, 53810 Lappeenranta", 2000);
+            office.PrintInfo();
+
+            // create object using copy consructor
+            Company postOffice = new Company(office);
+            postOffice.PrintInfo();
+
         }
     }
 }
