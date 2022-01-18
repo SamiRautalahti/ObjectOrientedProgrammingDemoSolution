e52f499476fdfa3392e47f3d08a6b4c66eecf168
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-18 10:06:52 +0200

    Company Exercise

---
+++ CompanyExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Company tehtävä:");
+        }
+    }
+}
