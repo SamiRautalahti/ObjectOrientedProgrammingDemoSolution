879629a693cf474a4dd1b83b076cad7b6e28c7e5
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-21 14:07:16 +0200

    Employee exercise

---
+++ EmployeeExercise/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExercise
+{
+    class Student
+    {
+        public string firstName;
+        public string lastName;
+
+        public Student()
+        {
+        }
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.lastName} {this.firstName}";
+        }
+    }
+}
