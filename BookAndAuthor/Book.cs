6424ecc727d394cdd431adf1a24bd8ef65fbe88e
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-01-28 21:39:01 +0200

    Update 28.1.2022

--- BookAndAuthor/Book.cs
+++ BookAndAuthor/Book.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BookOld
+namespace BookAndAuthor
 {
     class Book
     {
--- BookAndAuthor/Program.cs
+++ BookAndAuthor/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Literature;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
