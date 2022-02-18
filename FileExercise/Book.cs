6a0a4e8c87bb027c68c00fe0596359ef473d2144
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-02-18 14:16:59 +0200

    Update 14:16 18.2.2022

---
+++ FileExercise/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExercise
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        // empty constructor
+        public Book()
+        {
+            this.title = string.Empty;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 0;
+        }
+
+        // constructor
+        public Book(string title)
+        {
+            this.title = title;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 0;
+        }
+
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            SetId(id);
+            this.price = price;
+        }
+
+        public string GetBookInformation()
+        {
+            //return this.author + ", " + this.title +", " + this.id +", "+ this.price;
+            return $"{this.author}, {this.title}, {this.id}, {this.price:F2} EUR";
+        }
+
+        public void SetId(string idValue)
+        {
+            //tarkistaa tässä onko id:n pituus viisi merkkiä
+            if (idValue.Length == 5)
+            {
+                this.id = idValue;
+            }
+            else
+            {
+                this.id = "not valid";
+            }
+            
+        }
+        public void CompareBook(Book book)
+        {
+         
+            //verrata parametrinä olevan olion kappalehintaa kutsuttavan kappalehintaan
+            // palauta tieto siitä, kumpi kirja on kalliimpi.
+            if (this.price > book.price)
+            {
+                Console.WriteLine($"{this.title} on kalliimpi kuin {book.title}");
+            }
+            else if (this.price < book.price)
+            {
+                Console.WriteLine($"{book.title} on kalliimpi kuin {this.title}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.title} on samanhintainen kuin {book.title}");
+            }
+            Console.WriteLine($"kutsuva olio {this.title}");
+            Console.WriteLine($"parametrina tuleva olio {book.title}");
+        }
+
+        public int CompareTo(Book other)
+        {
+            return this.price.CompareTo(other.price);
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileExercise/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string filePath = @"C:\Users\sampp\Source\Repos\SamiRautalahti\ObjectOrientedProgrammingDemoSolution\FileExercise\testfile.txt";
+
+            FileManager fm = new FileManager(filePath);
+            string text = fm.ReadWords();
+            Console.WriteLine(text);
+
+            WriteFile();
+            ReadFile();
+            WriteJsonFile();
+            ReadJsonFile();
+        }
+
+            static void WriteFile()
+            {
+                Console.WriteLine("\nWriting to file data.txt");
+
+                const string path = @"C:\Users\sampp\Source\Repos\SamiRautalahti\ObjectOrientedProgrammingDemoSolution\FileExercise\data.txt";
+
+                // File.Exists(string path);
+                if (File.Exists(path))
+                {
+                string[] createText = { "Hello", "And", "Welcome" };
+                File.WriteAllLines(path, createText, Encoding.UTF8);
+                // File.WriteAllLines(string path, string[] contents, System.Text.Encoding encoding);
+
+                string appendText = "This is extra text" + Environment.NewLine;
+                File.AppendAllText(path, appendText, Encoding.UTF8);
+                // File.AppendAllText(string path, string contents);
+            }
+
+            
+
+        }
+
+            static void ReadFile()
+            {
+            Console.WriteLine("\nReading file data.txt");
+
+            const string path = @"C:\Users\sampp\Source\Repos\SamiRautalahti\ObjectOrientedProgrammingDemoSolution\FileExercise\data.txt";
+
+            // File.ReadAllLines(string path);
+            if (File.Exists(path))
+                {
+                string[] readText = File.ReadAllLines(path, Encoding.UTF8);
+                foreach (string s in readText)
+                {
+                    Console.WriteLine(s);
+                }
+                
+            }
+
+        }
+
+        static void WriteJsonFile()
+        {
+            Console.WriteLine("\nWriting Json data to books.txt file");
+
+            const string jsonFile = @"C:\Users\sampp\Source\Repos\SamiRautalahti\ObjectOrientedProgrammingDemoSolution\FileExercise\books.txt";
+
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book()
+            {
+                title = "Fingerpori I",
+                author = "Pertti Jarla",
+                id = "9789511355137",
+                price = 17.95
+            });
+            bookList.Add(new Book()
+            {
+                title = "Fingerpori II",
+                author = "Pertti Jarla",
+                id = "9789511355138",
+                price = 19.95
+            });
+            bookList.Add(new Book()
+            {
+                title = "Fingerpori III",
+                author = "Pertti Jarla",
+                id = "9789511355139",
+                price = 21.95
+            });
+            bookList.Add(new Book()
+            {
+                title = "Fingerpori IV",
+                author = "Pertti Jarla",
+                id = "9789511355140",
+                price = 23.95
+            });
+            File.WriteAllText(@"C:\Users\sampp\Source\Repos\SamiRautalahti\ObjectOrientedProgrammingDemoSolution\FileExercise\books.txt", JsonConvert.SerializeObject(bookList));
+        }
+
+            
+            // JsonConvert.SerializeObject(Object value);
+
+            //List<Book> booklist = new List<Book>();
+            //booklist.Add(new Book("Fingerpori I", "Pertti Jarla", "9789511355137", 17.95));
+            //booklist.Add(new Book("Fingerpori II", "Pertti Jarla", "9789511355138", 19.95));
+            //booklist.Add(new Book("Fingerpori III", "Pertti Jarla", "9789511355139", 21.95));
+            //booklist.Add(new Book("Fingerpori IV", "Pertti Jarla", "9789511355140", 23.95));
+
+
+
+            // File.WriteAllText(string path, string contents);
+  
+            
+                // Create a file to write to.
+                
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+           
+
+            
+
+            // Serialize JSON to a file
+            // https://www.newtonsoft.com/json/help/html/SerializeWithJsonSerializerToFile.htm
+
+        
+
+            static void ReadJsonFile()
+            {
+                Console.WriteLine("\nReading Json data from books.txt file");
+                
+                // File.ReadAllText(string path, E
+
+
+                Console.ReadLine();
+            }
+        }
+    }
