bf4207f351a0e3f372be5595b030fbeb3e4b0a6f
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-02-23 16:09:24 +0200

    Updated 16:09 23.2.2022

---
+++ Luokka Storage/IStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luokka_Storage
+{
+    interface IStorage {
+
+        // lisää varastoon parametrin ilmoittaman määrän tavaroita
+        string AddItems(int amount);
+        // palauttaa varastossa olevan vapaan tilan määrän
+        int GetFreeSpace();
+        // poistaa varastosta parametrin ilmoittaman määrän tavaroita
+        string RemoveItems(int amount);
+        // tyhjentää varaston
+        void ClearStorage();
+    }
+}
